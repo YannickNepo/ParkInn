c58a6e8428f03b1f78d169118200586d17e98d8d
YannickNepo <nepoyannick@gmail.com>
2023-06-26 18:47:32 -0300

    Login/registrarte

--- src/Paginas/Login/Login.js
+++ src/Paginas/Login/Login.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import './Login.css';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
-
 export default function LoginPage(){
+  const history = useNavigate();
 
+  const redirectToAnotherRoute = () => {
+    history('/');
+  };
 
+  return(
+    <div className='contenedor-login'>
+      <div className='info'>
+       
+        <h1 className='titulo'>INICIO DE SESIÓN
+          <hr className='barra-abajo'></hr>
+        </h1>
+        <div className='contenedor-texto-login'>
+          <input className='input-login'type='text' placeholder="Nombre de usuario"></input>
+          <hr className='barra-input'></hr>
+          <input className='input-login'type='password' placeholder="Contraseña"></input>
+          <hr className='barra-input'></hr>
+          <h5>No tienes cuenta? <Link to="/register">Registrate</Link></h5>
+        </div>
+        <button type="button" className="boton" onClick={redirectToAnotherRoute}>
+          <p className="boton-texto">&nbsp;INICIAR SESION</p>
+        </button>
+      </div>
+    </div>
+  );
 }
--- src/Paginas/Registrarse/Registrarse.js
+++ src/Paginas/Registrarse/Registrarse.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import './Registrarse.css';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
+export default function RegisterPage(){
 
-export default function RegistrarsePage(){
+  const history = useNavigate();
 
+  const redirectToAnotherRoute = () => {
+    history('/');
+  };
 
+  return(
+    <div className='contenedor-login'>
+      <div className='info'>
+        <h1 className='titulo'>Crear cuenta
+        </h1>
+        <div className='contenedor-texto-login'>
+            <input className='input-login' type='text' placeholder="Nombre de usuario"></input>
+          <hr className='barra-input'></hr>
+          <input className='input-login' type='mail' placeholder="Email"></input>
+          <hr className='barra-input'></hr>
+          <div>
+            <input className='input-login' type='password' placeholder="Contraseña"></input> 
+          </div>
+          <hr className='barra-input'></hr>
+          <h5>Ya tienes cuenta? <Link to="/login">Inicia sesión</Link></h5>   
+        </div>
+        <button type="button" className="boton" onClick={redirectToAnotherRoute}>
+          <p className="boton-texto">&nbsp;&nbsp;REGISTRARSE</p>
+        </button>
+       </div>
+     </div>
+   );
 }
   
