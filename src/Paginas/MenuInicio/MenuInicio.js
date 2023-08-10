47c29a2f9be491f55d86e60e6160e50f5f5f88d5
YannickNepo <nepoyannick@gmail.com>
2023-08-10 14:12:32 -0300

    error solucionado 10/8

--- src/Paginas/MenuInicio/MenuInicio.js
+++ src/Paginas/MenuInicio/MenuInicio.js
@@ -1,18 +1,21 @@
-import React from "react";
-import './Paginas/MenuInicio.css'
+import React from 'react';
+import './MenuInicio.css';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import logo from '../../assets/proyecto.png';
 
-return (
-   
-    <div className='Inicio'>
+export default function MenIni(){
+  const history = useNavigate();
+
+  const redirectToAnotherRoute = () => {
+    history('/BuscarEstacionamiento');
+
+  return(
+    <div className='Home'>
       <div className="Logo" >
       <img src={logo} alt="LogoPark" />
-    </div>
+     </div>
     </div>
   );
-
-
-
-  export default Home;
-
-
+}
+}
