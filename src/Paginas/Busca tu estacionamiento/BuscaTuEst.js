1e26baf48aedbc299788ff2ea028eb4753e227b5
YannickNepo <nepoyannick@gmail.com>
2023-11-13 15:52:38 -0300

    PE ERROR BOTON PUBLICAR

--- src/Paginas/Busca tu estacionamiento/BuscaTuEst.js
+++ src/Paginas/Busca tu estacionamiento/BuscaTuEst.js
@@ -1,45 +1,91 @@
-import React from "react";
+import React, { useState } from 'react';
 import './BuscaTuEst.css';
 import logo from '../../assets/proyecto.png';
 import { useNavigate } from 'react-router-dom';
 
+const UbicacionActual = ({ onLocationChange }) => {
+  const [currentLocation, setCurrentLocation] = useState(null);
 
-export default function BuscaEstacionamiento(){
-  const history = useNavigate();
-  const redirectToMenuInicio = () => {
-    history('/MenuInicio');
-  }
+  const handleGetLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        async (position) => {
+          const { latitude, longitude } = position.coords;
 
-  return(
-    <div className='contenedor-BE'>
-      <div className="LogoBE" >
-      <button type="button" className="btn_logo" onClick={redirectToMenuInicio}>
-      <img src={logo} alt="Botonlogo" />
-      </button>
-      </div>
-      <div className='info'>
-        <h1 className='tituloBE'>Busca estacionamiento por tu zona
-        </h1>
-        <div className='contenedor-texto-BE'>
-          <label className='Barrio' >Seleccione su barrio</label>
-    <select className="barrios">
-        <option value="Palermo">Palermo</option>
-        <option value="Belgrano">Belgrano</option>
-        <option value="Nuñez">Nuñez</option>
-        <option value="Almagro">Almagro</option>
-    </select> 
-          <button type="button" className="botonPublicar" onClick={redirectToMenuInicio}>
-          <p className="boton-texto">&nbsp;Buscar</p>
-        </button>
-          
+          // Utiliza la API de geocodificación de Google Maps para obtener la dirección
+          const response = await fetch(
+            `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=TU_CLAVE_DE_API_DE_GOOGLE_MAPS`
+          );
 
-    </div>
-    </div>
+          if (response.ok) {
+            const data = await response.json();
+            const address = data.results[0].formatted_address;
+            setCurrentLocation(address);
+            onLocationChange(address);
+          } else {
+            console.error('Error al obtener la dirección desde la API de Google Maps');
+          }
+        },
+        (error) => {
+          console.error('Error al obtener la ubicación:', error);
+        }
+      );
+    } else {
+      console.error('El navegador no soporta la geolocalización');
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleGetLocation}>Utilizar Ubicación Actual</button>
+      {currentLocation && <p>Dirección actual: {currentLocation}</p>}
     </div>
   );
-  } 
+};
 
+export default function BuscaEstacionamiento() {
+  const history = useNavigate();
+  const [selectedBarrio, setSelectedBarrio] = useState('');
+  const [currentLocation, setCurrentLocation] = useState('');
 
+  const redirectToMenuInicio = () => {
+    history('/MenuInicio');
+  };
 
+  const handleBuscarClick = () => {
+    // Puedes utilizar selectedBarrio y currentLocation en tu lógica de búsqueda
+    console.log('Barrio seleccionado:', selectedBarrio);
+    console.log('Ubicación actual:', currentLocation);
+    // Realiza la búsqueda o cualquier otra lógica aquí
+  };
 
-  
+  return (
+    <div className='contenedor-BE'>
+      <div className="LogoBE">
+        <button type="button" className="btn_logo" onClick={redirectToMenuInicio}>
+          <img src={logo} alt="Botonlogo" />
+        </button>
+      </div>
+      <div className='info'>
+        <h1 className='tituloBE'>Busca estacionamiento por tu zona</h1>
+        <div className='contenedor-texto-BE'>
+          <label className='Barrio'>Seleccione su barrio</label>
+          <select
+            className="barrios"
+            value={selectedBarrio}
+            onChange={(e) => setSelectedBarrio(e.target.value)}
+          >
+            <option value="Palermo">Palermo</option>
+            <option value="Belgrano">Belgrano</option>
+            <option value="Nuñez">Nuñez</option>
+            <option value="Almagro">Almagro</option>
+          </select>
+          <UbicacionActual onLocationChange={(location) => setCurrentLocation(location)} />
+          <button type="button" className="botonPublicar" onClick={handleBuscarClick}>
+            <p className="boton-texto">&nbsp;Buscar</p>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Paginas/Publica tu estacionamiento/PublicaEstacionamiento.js
+++ src/Paginas/Publica tu estacionamiento/PublicaEstacionamiento.js
@@ -1,31 +1,53 @@
-import React, { useState } from "react";
+import React from "react";
 import './PublicaEstacionamiento.css';
 import logo from '../../assets/proyecto.png';
 import { useNavigate } from 'react-router-dom';
 
 export default function PublicaEstacionamiento() {
   const history = useNavigate();
+
   const redirectToMenuInicio = () => {
     history('/MenuInicio');
   }
 
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [previewImage, setPreviewImage] = useState(null);
-  const [isTextVisible, setIsTextVisible] = useState(true);
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
 
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-    setSelectedFile(file);
-    const reader = new FileReader();
-    reader.onload = () => {
-      setPreviewImage(reader.result);
-      setIsTextVisible(false); // Oculta el texto después de seleccionar la imagen
+    const direccion = document.querySelector('.input-Direccion').value;
+    const capacidad = document.querySelector('.input-Capacidad').value;
+    const contacto = document.querySelector('.input-Contacto').value;
+    const tipo = document.querySelector('.input-Tipo').value;
+    const fechaDesde = document.querySelector('.input-Fecha1').value;
+    const fechaHasta = document.querySelector('.input-Fecha2').value;
+
+    const formData = {
+      direccion,
+      capacidad,
+      contacto,
+      tipo,
+      fechaDesde,
+      fechaHasta,
+    };
+
+    try {
+      const response = await fetch('https://fair-teal-clownfish.cyclic.app/CreateParking', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        // El envío fue exitoso, redirige a la página deseada.
+        console.log('Datos enviados exitosamente');
+        history('/MenuInicio');
+      } else {
+        console.error('Error al enviar los datos al servidor');
+      }
+    } catch (error) {
+      console.error('Error al realizar la solicitud fetch:', error);
     }
-    reader.readAsDataURL(file);
-  }
-
-  const handleFormSubmit = async () => {
-    // ... (código para enviar la imagen al servidor)
   }
 
   return (
@@ -37,44 +59,25 @@ export default function PublicaEstacionamiento() {
       </div>
       <div className='info'>
         <div className='contenedor-texto-PE'>
-          <label className='Dire'>Seleccione su dirección</label>
-          <input className='input-Direccion' type='text' placeholder="" />
-          <label className='Capacidad'>Capacidad del estacionamiento</label>
-          <input className='input-Capacidad' type='number' placeholder="" />
-          <label className='Contacto'>Número de contacto</label>
-          <input className='input-Contacto' type='tel' placeholder="" />
-          <div>
-            <label className='Tipo'>Tipo del estacionamiento</label>
-            <input className='input-Tipo' type='text' placeholder="Por ejemplo: Techado" />
-            <label className='Fecha1'>Desde</label>
-            <input className='input-Fecha1' type='date' placeholder="" />
-            <label className='Fecha2'>Hasta</label>
-            <input className='input-Fecha2' type='date' placeholder="" />
-          </div>
-          <div>
-            <input
-              className='input-Foto'
-              type='file'
-              accept="image/*"
-              onChange={handleFileChange}
-            />
-            {isTextVisible && !previewImage && (
-              <p>Por favor, elige una imagen</p>
-            )}
-            {previewImage && (
-              <div>
-                <img
-                  className="preview-image"
-                  src={previewImage}
-                  alt="Preview"
-                  style={{ maxWidth: '100%', maxHeight: '400px' }} // Ajusta el tamaño de la imagen para adaptarlo a la pantalla
-                />
-              </div>
-            )}
-            <button type="button" className="botonPublicar" onClick={handleFormSubmit}>
-              <p className="boton-texto">&nbsp;Publicar</p>
-            </button>
-          </div>
+          <form onSubmit={handleFormSubmit}>
+            <label className='Dire'>Seleccione su dirección</label>
+            <input className='input-Direccion' type='text' placeholder="" />
+            <label className='Capacidad'>Capacidad del estacionamiento</label>
+            <input className='input-Capacidad' type='number' placeholder="" />
+            <label className='Contacto'>Mail de contacto</label>
+            <input className='input-Contacto' type='text' placeholder="" />
+            <div>
+              <label className='Tipo'>Tipo del estacionamiento</label>
+              <input className='input-Tipo' type='text' placeholder="Por ejemplo: Techado" />
+              <label className='Fecha1'>Desde</label>
+              <input className='input-Fecha1' type='date' placeholder="" />
+              <label className='Fecha2'>Hasta</label>
+              <input className='input-Fecha2' type='date' placeholder="" />
+            </div>
+              <button type="submit" className="botonPublicar">
+                <p className="boton-texto">&nbsp;Publicar</p>
+              </button>
+          </form>
         </div>
       </div>
     </div>
